4677519de5181ef3080de67f5b6bca0021a224e8
Nicolas Rodriguez Lasso <nicolas.rlasso@gmail.com>
2021-04-28 10:16:30 -0500

    add controller

--- demo/src/main/java/com/example/web/HelloController.java
+++ demo/src/main/java/com/example/web/HelloController.java
@@ -1,10 +1,13 @@
 package com.example.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String sayHello() {
-		return "hello";
+		return "<h1>hello world</h1>";
 	}
 }
